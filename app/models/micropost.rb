da99f091c2a0dedd574f50de44dbd84a719996ae
shundai.ogawa <ec2-user@ip-172-31-43-33.us-east-2.compute.internal>
2018-04-06 12:26:21 +0000

    favorite function

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,6 @@ class Micropost < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
+  has_many :reverses_of_favorite, class_name: 'Favorite'
+  has_many :liked, through: :reverses_of_favorite, source: :user
 end
